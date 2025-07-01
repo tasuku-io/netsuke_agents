b884baba851de17dd987f8dd0226391d66fb6845
Luis Guzman <luisdaniel.guzmanp@gmail.com>
2025-07-01 12:14:24 +0900

    loads openai api key from consuming app env if found

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,6 +3,13 @@ import Dotenvy
 
 source!(".env")
 
+api_key = case System.get_env("OPENAI_API_KEY") do
+  nil ->
+    env!("OPENAI_API_KEY")
+  key when is_binary(key) ->
+    key
+end
+
 config :instructor,
   adapter: Instructor.Adapters.OpenAI,
-  openai: [api_key: env!("OPENAI_API_KEY")]
+  openai: [api_key: api_key]
